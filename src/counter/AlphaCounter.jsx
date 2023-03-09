94e23125f54ff0cbc6afb26209e2c121c6f6883a
Guillermo Cordova <guiller_ch@hotmail.com>
2023-03-09 01:54:56 -0400

    se redujo el margen de aparicion para incrementar la exactitud

--- src/counter/AlphaCounter.jsx
+++ src/counter/AlphaCounter.jsx
@@ -53,7 +53,7 @@ export const AlphaCounter = () => {
 
     return (
         <div>
-        {(horas == 0 && minutos < 30) || (horas >= 4 && minutos >= 30) ? (
+        {(horas == 0 && minutos < 2) || (horas >= 4 && minutos >= 30) ? (
             <p className="alert alert-danger text-center">
             <b>Podria estar un pokemon Alpha rondando ahora mismo!!</b>
             </p>
