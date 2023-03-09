b7d22b5cfcd58f7ccafa005f2bfedb08e0a6e0c9
Guillermo Cordova <guiller_ch@hotmail.com>
2023-03-09 13:14:27 -0400

    se modifico el algoritmo de horas y la condicion de mensaje

--- src/counter/AlphaCounter.jsx
+++ src/counter/AlphaCounter.jsx
@@ -4,68 +4,57 @@ import moment from "moment-timezone";
 export const AlphaCounter = () => {
     const zonaHoraria = "America/La_Paz";
 
-    const obtenerFechaReferencia = () => {
-        return moment.tz("2023-03-08T21:00:00", zonaHoraria);
-    };
+    const horaActual = moment().tz(zonaHoraria).format("HH:mm:ss");
+    const horaReal = parseInt(horaActual.split(":")[0]);
 
-    const [fechaReferencia, setFechaReferencia] = useState(
-        obtenerFechaReferencia()
-    );
+    const [segundosRestantes, setSegundosRestantes] = useState(0);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-        setFechaReferencia(obtenerFechaReferencia());
-        }, 60 * 60 * 1000);
+        const fechaActual = moment.tz(zonaHoraria);
+        const horaActual = fechaActual.clone().startOf("minute");
+        let fechaEvento = horaActual.clone();
 
-        return () => clearInterval(intervalId);
-    }, []);
+        if (horaActual.hours() >= 21 || horaActual.hours() < 6) {
+            fechaEvento = fechaEvento.add(9, "hours").add(20, "minutes");
+        } else if (horaActual.hours() >= 16) {
+            fechaEvento = fechaEvento.add(5, "hours").add(20, "minutes");
+        } else if (horaActual.hours() >= 12) {
+            fechaEvento = fechaEvento.add(4, "hours").add(20, "minutes");
+        } else {
+            fechaEvento = fechaEvento.add(6, "hours").add(20, "minutes");
+        }
 
-    const [ultimoEvento, setUltimoEvento] = useState(
-        fechaReferencia.toDate()
-    );
-
-    const fechaInicio = moment(fechaReferencia)
-        .add(5, "hours")
-        .toDate();
-
-    const [segundosRestantes, setSegundosRestantes] = useState(
-        Math.floor((fechaInicio - new Date()) / 1000)
-    );
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-        setSegundosRestantes((prevSegundos) => prevSegundos - 1);
+        const segundosRestantes = fechaEvento.diff(fechaActual, "seconds");
+        setSegundosRestantes(segundosRestantes);
         }, 1000);
 
         return () => clearInterval(intervalId);
     }, []);
 
-    useEffect(() => {
-        if (segundosRestantes <= 0) {
-        setUltimoEvento(new Date());
-        setSegundosRestantes(5 * 60 * 60);
-        }
-    }, [segundosRestantes]);
-
     const horas = Math.floor(segundosRestantes / 3600);
     const minutos = Math.floor((segundosRestantes % 3600) / 60);
     const segundos = segundosRestantes % 60;
-
+    
     return (
         <div>
-        {(horas == 0 && minutos < 2) || (horas >= 4 && minutos >= 30) ? (
+        {
+            (horaReal >= 6 && horaReal <= 7) ||
+            (horaReal >= 12 && horaReal <= 13) ||
+            (horaReal >= 16 && horaReal <= 17) ||
+            (horaReal >= 21 && horaReal <= 22)
+        ? (
             <p className="alert alert-danger text-center">
-            <b>Podria estar un pokemon Alpha rondando ahora mismo!!</b>
+                <b>Podria estar un pokemon Alpha rondando ahora mismo!!</b>
             </p>
-        ) : (
+            ) 
+        : (
             <p className="alert alert-primary text-center">
-            {`Tiempo aproximado hasta el proximo Alpha ${horas
-                .toString()
-                .padStart(2, "0")}:${minutos.toString().padStart(2, "0")}:${segundos
-                .toString()
-                .padStart(2, "0")}`}
+            {`Tiempo aproximado hasta el proximo Alpha 
+            ${horas.toString().padStart(2, "0")}:${minutos.toString().padStart(2, "0")}:${segundos.toString().padStart(2, "0")}`}
             </p>
-        )}
+            )
+        }
         </div>
     );
 };
