3a2586d15b2e67236ea654f173fc79f723e2fa7c
Guillermo Cordova <guiller_ch@hotmail.com>
2023-03-08 22:06:21 -0400

    se realizo ajuste de horas a 5

--- src/counter/AlphaCounter.jsx
+++ src/counter/AlphaCounter.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 export const AlphaCounter = () => {
-    const [ultimoEvento, setUltimoEvento] = useState(new Date("2023-03-08T16:28:00"));
+    const [ultimoEvento, setUltimoEvento] = useState(new Date("2023-03-08T21:00:00"));
 
     const fechaInicio = new Date(ultimoEvento.getTime() + 5 * 60 * 60 * 1000);
 
