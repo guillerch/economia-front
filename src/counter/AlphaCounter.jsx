ff60abdee369863aed3c49f9f158cb416e739825
Guillermo Cordova <guiller_ch@hotmail.com>
2023-03-09 14:31:25 -0400

    se corrigio la condicion de alerta

--- src/counter/AlphaCounter.jsx
+++ src/counter/AlphaCounter.jsx
@@ -3,35 +3,40 @@ import moment from "moment-timezone";
 
 export const AlphaCounter = () => {
     const zonaHoraria = "America/La_Paz";
-
-    const horaActual = moment().tz(zonaHoraria).format("HH:mm:ss");
-    const horaReal = parseInt(horaActual.split(":")[0]);
-    const minutoReal = parseInt(horaActual.split(":")[1]);
-    
+    const fechaActual = moment.tz(zonaHoraria);
+    const horaActual = fechaActual.hour();
+    const minutoActual = fechaActual.minute();
+    const segundoActual = fechaActual.second();
     const [segundosRestantes, setSegundosRestantes] = useState(0);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-        const fechaActual = moment.tz(zonaHoraria);
-        const horaActual = fechaActual.clone().startOf("minute");
-        let fechaEvento = horaActual.clone();
+            
 
-        if (horaActual.hours() >= 21 || horaActual.hours() < 6) {
-            fechaEvento = fechaEvento.add(9, "hours").add(20, "minutes");
-        } else if (horaActual.hours() >= 16) {
-            fechaEvento = fechaEvento.add(5, "hours").add(20, "minutes");
-        } else if (horaActual.hours() >= 12) {
-            fechaEvento = fechaEvento.add(4, "hours").add(20, "minutes");
-        } else {
-            fechaEvento = fechaEvento.add(6, "hours").add(20, "minutes");
-        }
+            const horaProximoEvento = (() => {
+            if (horaActual >= 21 || horaActual < 6) {
+                return { hora: 6, minuto: 20 };
+            } else if (horaActual >= 16) {
+                return { hora: 21, minuto: 20 };
+            } else if (horaActual >= 12) {
+                return { hora: 16, minuto: 20 };
+            } else {
+                return { hora: 12, minuto: 20 };
+            }
+            })();
 
-        const segundosRestantes = fechaEvento.diff(fechaActual, "seconds");
-        setSegundosRestantes(segundosRestantes);
+            const fechaProximoEvento = fechaActual
+            .clone()
+            .hour(horaProximoEvento.hora)
+            .minute(horaProximoEvento.minuto)
+            .second(0);
+
+            const segundosRestantes = fechaProximoEvento.diff(fechaActual, "seconds");
+            setSegundosRestantes(segundosRestantes);
         }, 1000);
 
         return () => clearInterval(intervalId);
-    }, []);
+        }, []);
 
     const horas = Math.floor(segundosRestantes / 3600);
     const minutos = Math.floor((segundosRestantes % 3600) / 60);
@@ -40,10 +45,10 @@ export const AlphaCounter = () => {
     return (
         <div>
         {
-            ((horaReal === 6 || horaReal === 7) && (minutoReal<=59)) ||
-            ((horaReal === 12 || horaReal === 13) && (minutoReal<=59)) ||
-            ((horaReal === 16 || horaReal === 17) && (minutoReal<=59)) ||
-            ((horaReal === 21 || horaReal === 22) && (minutoReal<=59))
+            ((horaActual === 6 || horaActual === 7) && (minutoReal<=59)) ||
+            ((horaActual === 12 || horaActual === 13) && (minutoReal<=59)) ||
+            ((horaActual === 16 || horaActual === 17) && (minutoReal<=59)) ||
+            ((horaActual === 21 || horaActual === 22) && (minutoReal<=59))
         ? (
             <p className="alert alert-danger text-center">
                 <b>Podria estar un pokemon Alpha rondando ahora mismo!!</b>
