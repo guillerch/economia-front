8b19c1b1898be85a40b4d2363950cf94298a946c
Guillermo Cordova <guiller_ch@hotmail.com>
2023-04-04 15:56:27 -0400

    se agrego de manera local la data recibida desde la api y se agregaron las imagenes de manera local en formato webp

---
+++ src/data/bayas.js
@@ -0,0 +1,642 @@
+export const bayasData = [
+    {
+        "id": 1,
+        "name": "Zreza",
+        "time": 16,
+        "description": "Cura la paralisis",
+        "seed1": "Picante",
+        "seed2": "Picante",
+        "seed3": "Picante",
+        "imagen": "/zreza.webp"
+    },
+    {
+        "id": 2,
+        "name": "Zanama",
+        "time": 20,
+        "description": "Restaura 10 PP de un movimiento",
+        "seed1": "Muy picante",
+        "seed2": "Dulce",
+        "seed3": "Amarga",
+        "imagen": "/zanama.webp"
+    },
+    {
+        "id": 3,
+        "name": "Enigma",
+        "time": 42,
+        "description": "Restaura 1/4 de los PS cuando el Pokémon es atacado con ataques super eficaces",
+        "seed1": "Muy picante",
+        "seed2": "Muy Picante",
+        "seed3": "Muy Seca",
+        "imagen": "/enigma.webp"
+    },
+    {
+        "id": 4,
+        "name": "Ziuela",
+        "time": 44,
+        "description": "Cura cualquier problema de estado",
+        "seed1": "Muy picante",
+        "seed2": "Muy Seca",
+        "seed3": "Muy Dulce",
+        "imagen": "/ziuela.webp"
+    },
+    {
+        "id": 5,
+        "name": "Zonlan",
+        "time": 67,
+        "description": "Aumenta la probabilidad de dar un golpe critico al tener menos de 1/3 de los PS",
+        "seed1": "Muy Picante",
+        "seed2": "Muy Dulce",
+        "seed3": "Muy Acida",
+        "imagen": "/zonlan.webp"
+    },
+    {
+        "id": 6,
+        "name": "Atania",
+        "time": 16,
+        "description": "Despierta al Pokémon",
+        "seed1": "Seca",
+        "seed2": "Seca",
+        "seed3": "Seca",
+        "imagen": "/atania.webp"
+    },
+    {
+        "id": 7,
+        "name": "Meloc",
+        "time": 16,
+        "description": "Cura el envenenamiento",
+        "seed1": "Dulce",
+        "seed2": "Dulce",
+        "seed3": "Dulce",
+        "imagen": "/meloc.webp"
+    },
+    {
+        "id": 8,
+        "name": "Safre",
+        "time": 16,
+        "description": "Cura las quemaduras",
+        "seed1": "Amarga",
+        "seed2": "Amarga",
+        "seed3": "Amarga",
+        "imagen": "/safre.webp"
+    },
+    {
+        "id": 9,
+        "name": "Perasi",
+        "time": 16,
+        "description": "Cura el congelamiento",
+        "seed1": "Amarga",
+        "seed2": "Amarga",
+        "seed3": "Amarga",
+        "imagen": "/perasi.webp"
+    },
+    {
+        "id": 10,
+        "name": "Acardo",
+        "time": 42,
+        "description": "Si la lleva un Pokémon, debilita un ataque super eficaz de tipo Tierra de un enemigo.",
+        "seed1": "Muy Picante",
+        "seed2": "Muy Dulce",
+        "seed3": "NN",
+        "imagen": "/Acardo.webp"
+    },
+    {
+        "id": 11,
+        "name": "Alcho",
+        "time": 42,
+        "description": "Si la lleva un Pokemon, debilita un ataque supereficaz de tipo Roca de un enemigo.",
+        "seed1": "Muy Picante",
+        "seed2": "Muy Seca",
+        "seed3": "NN",
+        "imagen": "/Alcho.webp"
+    },
+    {
+        "id": 12,
+        "name": "Algama",
+        "time": 44,
+        "description": "Al usarla con un Pokémon, se gana su amistad, pero también reduce su Ataque de base al subir de nivel.\r\nBaja 10 EVs (Puntos de Esfuerzo) de Ataque.",
+        "seed1": "Muy Seca",
+        "seed2": "Muy Acida",
+        "seed3": "NN",
+        "imagen": "/Algama.webp"
+    },
+    {
+        "id": 13,
+        "name": "Andano",
+        "time": 42,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los Gourmets.",
+        "seed1": "Muy Acida",
+        "seed2": "Picante",
+        "seed3": "NN",
+        "imagen": "/Andano.webp"
+    },
+    {
+        "id": 14,
+        "name": "Ango",
+        "time": 20,
+        "description": "Los Pokemon pueden usarla para restaurar algunos PS. Su sabor puede causar confusión.",
+        "seed1": "Muy Dulce",
+        "seed2": "Muy Dulce",
+        "seed3": "NN",
+        "imagen": "/Ango.webp"
+    },
+    {
+        "id": 15,
+        "name": "Anjiro",
+        "time": 42,
+        "description": "Si la lleva un Pokémon, debilita un ataque super eficaz de tipo Dragón de un enemigo.",
+        "seed1": "Muy Dulce",
+        "seed2": "Muy Amarga",
+        "seed3": "NN",
+        "imagen": "/Anjiro.webp"
+    },
+    {
+        "id": 16,
+        "name": "Aostan",
+        "time": 20,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los Gourmets.",
+        "seed1": "Muy Dulce",
+        "seed2": "Amarga",
+        "seed3": "NN",
+        "imagen": "/Aostan.webp"
+    },
+    {
+        "id": 17,
+        "name": "Arabol",
+        "time": 67,
+        "description": "Si la lleva un Pokemón, aumenta mucho una característica en un momento de apuro.",
+        "seed1": "Muy Seca",
+        "seed2": "Muy Dulce",
+        "seed3": "Muy Amarga",
+        "imagen": "/Arabol.webp"
+    },
+    {
+        "id": 18,
+        "name": "Aranja",
+        "time": 16,
+        "description": "Los Pokemon pueden llevarla o usarla para restaurar 10 PS.",
+        "seed1": "Seca",
+        "seed2": "Amarga",
+        "seed3": "Acida",
+        "imagen": "/aranja.webp"
+    },
+    {
+        "id": 19,
+        "name": "Aricoc",
+        "time": 67,
+        "description": "Si la lleva un Pokemon, sube mucho la Defensa Especial en un momento de apuro.",
+        "seed1": "Muy Seca",
+        "seed2": "Muy Acida",
+        "seed3": "Picante",
+        "imagen": "/Aricoc.webp"
+    },
+    {
+        "id": 20,
+        "name": "Aslac",
+        "time": 67,
+        "description": "Si la lleva un Pokemon, sube mucho la Velocidad en un momento de apuro.",
+        "seed1": "Muy Dulce",
+        "seed2": "Muy Acida",
+        "seed3": "Amarga",
+        "imagen": "/Aslac.webp"
+    },
+    {
+        "id": 21,
+        "name": "Baribá",
+        "time": 42,
+        "description": "Si la lleva un Pokémon, debilita un ataque super eficaz de tipo Acero de un enemigo.",
+        "seed1": "Muy Picante",
+        "seed2": "Muy Seca",
+        "seed3": "NN",
+        "imagen": "/Barib%C3%A1.webp"
+    },
+    {
+        "id": 22,
+        "name": "Caoca",
+        "time": 42,
+        "description": "Si la lleva un Pokemon, debilita un ataque supereficaz de tipo Fuego de un enemigo.",
+        "seed1": "Muy Picante",
+        "seed2": "Muy Dulce",
+        "seed3": "NN",
+        "imagen": "/Caoca.webp"
+    },
+    {
+        "id": 23,
+        "name": "Caquic",
+        "time": 16,
+        "description": "Los Pokemon pueden llevarla o usarla para despertar del sueño.",
+        "seed1": "Picante",
+        "seed2": "Seca",
+        "seed3": "Dulce",
+        "imagen": "/Caquic.webp"
+    },
+    {
+        "id": 24,
+        "name": "Chilan",
+        "time": 42,
+        "description": "Si la lleva un Pokémon, debilita un ataque de tipo Normal de un enemigo.",
+        "seed1": "Muy Seca",
+        "seed2": "Muy Dulce",
+        "seed3": "NN",
+        "imagen": "/Chilan.webp"
+    },
+    {
+        "id": 25,
+        "name": "Chiri",
+        "time": 44,
+        "description": "El Pokémon que la lleva puede actuar en primer lugar una vez cuando se encuentra en un momento de apuro.",
+        "seed1": "Muy Dulce",
+        "seed2": "Muy Amarga",
+        "seed3": "NN",
+        "imagen": "/Chiri.webp"
+    },
+    {
+        "id": 26,
+        "name": "Dillo",
+        "time": 42,
+        "description": "Si la lleva un Pokemon, debilita un ataque supereficaz de tipo Siniestro de un enemigo.",
+        "seed1": "Muy Picante",
+        "seed2": "Muy Acida",
+        "seed3": "NN",
+        "imagen": "/Dillo.webp"
+    },
+    {
+        "id": 27,
+        "name": "Drasi",
+        "time": 42,
+        "description": "Si la lleva un Pokemon, debilita un ataque supereficaz de tipo Fantasma de un enemigo.",
+        "seed1": "Muy Seca",
+        "seed2": "Muy Dulce",
+        "seed3": "NN",
+        "imagen": "/Drasi.webp"
+    },
+    {
+        "id": 28,
+        "name": "Frambu",
+        "time": 16,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los gourmets.",
+        "seed1": "Muy Seca",
+        "seed2": "Picante",
+        "seed3": "NN",
+        "imagen": "/Frambu.webp"
+    },
+    {
+        "id": 29,
+        "name": "Gonlan",
+        "time": 67,
+        "description": "Si la lleva un Pokemon, sube mucho la Defensa en un momento de apuro.",
+        "seed1": "Muy Seca",
+        "seed2": "Muy Amarga",
+        "seed3": "Seca",
+        "imagen": "/Gonlan.webp"
+    },
+    {
+        "id": 30,
+        "name": "Grana",
+        "time": 44,
+        "description": "Al usarla con un Pokemon, se gana su amistad, pero también reduce su PS de base al subir de nivel.\r\nBaja 10 EVs (Puntos de Esfuerzo) de PS.",
+        "seed1": "Muy Picante",
+        "seed2": "Amarga",
+        "seed3": "NN",
+        "imagen": "/Grana.webp"
+    },
+    {
+        "id": 31,
+        "name": "Gualot",
+        "time": 42,
+        "description": "la lleva un Pokemon, debilita un ataque supereficaz de tipo Eléctrico de un enemigo.",
+        "seed1": "Muy Dulce",
+        "seed2": "Muy Amarga",
+        "seed3": "NN",
+        "imagen": "/Gualot.webp"
+    },
+    {
+        "id": 32,
+        "name": "Guaya",
+        "time": 20,
+        "description": "Los Pokemon pueden usarla para restaurar algunos PS. Su sabor puede causar confusión.",
+        "seed1": "Muy Amarga",
+        "seed2": "Muy Amarga",
+        "seed3": "NN",
+        "imagen": "/Guaya.webp"
+    },
+    {
+        "id": 33,
+        "name": "Higog",
+        "time": 20,
+        "description": "Los Pokémon pueden usarla para restaurar algunos PS. Su sabor puede causar confusión.",
+        "seed1": "Muy Picante",
+        "seed2": "Muy Picante",
+        "seed3": "NN",
+        "imagen": "/Higog.webp"
+    },
+    {
+        "id": 34,
+        "name": "Ispero",
+        "time": 44,
+        "description": "Al usarla con un Pokemon, se gana su amistad, pero también reduce su Defensa de base al subir de nivel.\r\nBaja 10 EVs (Puntos de Esfuerzo) de Defensa.",
+        "seed1": "Muy Dulce",
+        "seed2": "Picante",
+        "seed3": "NN",
+        "imagen": "/Ispero.webp"
+    },
+    {
+        "id": 35,
+        "name": "Jaboca",
+        "time": 44,
+        "description": "Si la lleva un Pokémon cuando otro le lanza un ataque físico, este último también recibe daño.",
+        "seed1": "Muy Amarga",
+        "seed2": "Muy Acida",
+        "seed3": "NN",
+        "imagen": "/Jaboca.webp"
+    },
+    {
+        "id": 36,
+        "name": "Kebia",
+        "time": 42,
+        "description": "Si la lleva un Pokémon, debilita un ataque super eficaz de tipo Veneno de un enemigo.",
+        "seed1": "Muy Seca",
+        "seed2": "Muy Acida",
+        "seed3": "NN",
+        "imagen": "/Kebia.webp"
+    },
+    {
+        "id": 37,
+        "name": "Kouba",
+        "time": 42,
+        "description": "Si la lleva un Pokemon, debilita un ataque supereficaz de tipo Volador de un enemigo.",
+        "seed1": "Muy Seca",
+        "seed2": "Muy Amarga",
+        "seed3": "NN",
+        "imagen": "/Kouba.webp"
+    },
+    {
+        "id": 38,
+        "name": "Lagro",
+        "time": 44,
+        "description": "Si la lleva un Pokemon, sube mucho la Precisión en un momento de apuro.",
+        "seed1": "Muy Seca",
+        "seed2": "Muy Dulce",
+        "seed3": "NN",
+        "imagen": "/Lagro.webp"
+    },
+    {
+        "id": 39,
+        "name": "Latano",
+        "time": 16,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los gourmets.",
+        "seed1": "Muy Dulce",
+        "seed2": "Amarga",
+        "seed3": "NN",
+        "imagen": "/Latano.webp"
+    },
+    {
+        "id": 40,
+        "name": "Lichi",
+        "time": 67,
+        "description": "Si la lleva un Pokemon, sube mucho el Ataque en un momento de apuro.",
+        "seed1": "Muy Picante",
+        "seed2": "Muy Dulce",
+        "seed3": "Seca",
+        "imagen": "/Lichi.webp"
+    },
+    {
+        "id": 41,
+        "name": "Magua",
+        "time": 44,
+        "description": "Si la lleva un Pokemón cuando otro le lanza un ataque especial, este último también recibe daño",
+        "seed1": "Muy Picante",
+        "seed2": "Muy Acida",
+        "seed3": "NN",
+        "imagen": "/Magua.webp"
+    },
+    {
+        "id": 42,
+        "name": "Mais",
+        "time": 20,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los Gourmets.",
+        "seed1": "Muy Seca",
+        "seed2": "Dulce",
+        "seed3": "NN",
+        "imagen": "/Mais.webp"
+    },
+    {
+        "id": 43,
+        "name": "Meluce",
+        "time": 44,
+        "description": "Al usarla con un Pokemon, se gana su amistad, pero también reduce su Ataque Especial de base al subir de nivel.\r\nBaja 10 EVs (Puntos de Esfuerzo) de Ataque Especial.",
+        "seed1": "Muy Amarga",
+        "seed2": "Seca",
+        "seed3": "NN",
+        "imagen": "/Meluce.webp"
+    },
+    {
+        "id": 44,
+        "name": "Monli",
+        "time": 20,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los Gourmets.",
+        "seed1": "Muy Acida",
+        "seed2": "Picante",
+        "seed3": "NN",
+        "imagen": "/Monli.webp"
+    },
+    {
+        "id": 45,
+        "name": "Oram",
+        "time": 16,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los gourmets.",
+        "seed1": "Muy Dulce",
+        "seed2": "Seca",
+        "seed3": "NN",
+        "imagen": "/Oram.webp"
+    },
+    {
+        "id": 46,
+        "name": "Pabaya",
+        "time": 20,
+        "description": "Los Pokemon pueden usarla para restaurar algunos PS. Su sabor puede causar confusión.",
+        "seed1": "Muy Acida",
+        "seed2": "Muy Acida",
+        "seed3": "NN",
+        "imagen": "/Pabaya.webp"
+    },
+    {
+        "id": 47,
+        "name": "Pasio",
+        "time": 42,
+        "description": "Si la lleva un Pokemon, debilita un ataque supereficaz de tipo Agua de un enemigo.",
+        "seed1": "Muy Seca",
+        "seed2": "Muy Amarga",
+        "seed3": "NN",
+        "imagen": "/Pasio.webp"
+    },
+    {
+        "id": 48,
+        "name": "Payapa",
+        "time": 42,
+        "description": "Si la lleva un Pokemon, debilita un ataque supereficaz de tipo Psiquico de un enemigo.",
+        "seed1": "Muy Dulce",
+        "seed2": "Muy Acida",
+        "seed3": "NN",
+        "imagen": "/Payapa.webp"
+    },
+    {
+        "id": 49,
+        "name": "Peragu",
+        "time": 16,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los gourmets.",
+        "seed1": "Muy Acida",
+        "seed2": "Amarga",
+        "seed3": "NN",
+        "imagen": "/Peragu.webp"
+    },
+    {
+        "id": 50,
+        "name": "Pinia",
+        "time": 16,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los gourmets.",
+        "seed1": "Muy Acida",
+        "seed2": "Picante",
+        "seed3": "NN",
+        "imagen": "/Pinia.webp"
+    },
+    {
+        "id": 51,
+        "name": "Plama",
+        "time": 42,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los Gourmets.",
+        "seed1": "Muy Seca",
+        "seed2": "Dulce",
+        "seed3": "NN",
+        "imagen": "/Plama.webp"
+    },
+    {
+        "id": 52,
+        "name": "Pomaro",
+        "time": 42,
+        "description": "Si la lleva un Pokemon, debilita un ataque supereficaz de tipo Lucha de un enemigo.",
+        "seed1": "Muy Picante",
+        "seed2": "Muy Amarga",
+        "seed3": "NN",
+        "imagen": "/Pomaro.webp"
+    },
+    {
+        "id": 53,
+        "name": "Rautan",
+        "time": 20,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los Gourmets.",
+        "seed1": "Muy Amarga",
+        "seed2": "Acida",
+        "seed3": "NN",
+        "imagen": "/Rautan.webp"
+    },
+    {
+        "id": 54,
+        "name": "Rimoya",
+        "time": 42,
+        "description": "Si la lleva un Pokemon, debilita un ataque supereficaz de tipo Hielo de un enemigo.",
+        "seed1": "Muy Seca",
+        "seed2": "Muy Acida",
+        "seed3": "NN",
+        "imagen": "/Rimoya.webp"
+    },
+    {
+        "id": 55,
+        "name": "Rudion",
+        "time": 42,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los Gourmets.",
+        "seed1": "Muy Amarga",
+        "seed2": "Acida",
+        "seed3": "NN",
+        "imagen": "/Rudion.webp"
+    },
+    {
+        "id": 56,
+        "name": "Sambia",
+        "time": 42,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los Gourmets.",
+        "seed1": "Muy Dulce",
+        "seed2": "Amarga",
+        "seed3": "NN",
+        "imagen": "/Sambia.webp"
+    },
+    {
+        "id": 57,
+        "name": "Tamar",
+        "time": 42,
+        "description": "Si la lleva un Pokémon, debilita un ataque super eficaz de tipo Planta de un enemigo.",
+        "seed1": "Muy Picante",
+        "seed2": "Muy Amarga",
+        "seed3": "NN",
+        "imagen": "/Tamar.webp"
+    },
+    {
+        "id": 58,
+        "name": "Tamate",
+        "time": 44,
+        "description": "Al usarla con un Pokemon, se gana su amistad, pero también reduce su Velocidad de base al subir de nivel.\r\nBaja 10 EVs (Puntos de Esfuerzo) de Velocidad.",
+        "seed1": "Muy Picante",
+        "seed2": "Seca",
+        "seed3": "NN",
+        "imagen": "/Tamate.webp"
+    },
+    {
+        "id": 59,
+        "name": "Uvav",
+        "time": 44,
+        "description": "Al usarla con un Pokemon, se gana su amistad, pero también reduce su Defensa Especial de base al subir de nivel.\r\nBaja 10 EVs (Puntos de Esfuerzo) de Defensa Especial.",
+        "seed1": "Muy Acida",
+        "seed2": "Dulce",
+        "seed3": "NN",
+        "imagen": "/Uvav.webp"
+    },
+    {
+        "id": 60,
+        "name": "Wikano",
+        "time": 42,
+        "description": "Una Baya muy rara en la región de Teselia. Muy apreciada por los Gourmets.",
+        "seed1": "Muy Picante",
+        "seed2": "Seca",
+        "seed3": "NN",
+        "imagen": "/Wikano.webp"
+    },
+    {
+        "id": 61,
+        "name": "Wiki",
+        "time": 20,
+        "description": "Los Pokemon pueden usarla para restaurar algunos PS. Su sabor puede causar confusión.",
+        "seed1": "Muy Seca",
+        "seed2": "Muy Seca",
+        "seed3": "NN",
+        "imagen": "/Wiki.webp"
+    },
+    {
+        "id": 62,
+        "name": "Yapati",
+        "time": 67,
+        "description": "Si la lleva un Pokemon, sube mucho el Ataque Especial en un momento de apuro.",
+        "seed1": "Muy Picante",
+        "seed2": "Muy Amarga",
+        "seed3": "Acida",
+        "imagen": "/Yapati.webp"
+    },
+    {
+        "id": 63,
+        "name": "Yecana",
+        "time": 42,
+        "description": "Si la lleva un Pokémon, debilita un ataque super eficaz de tipo Bicho de un enemigo.",
+        "seed1": "Muy Picante",
+        "seed2": "Muy Acida",
+        "seed3": "NN",
+        "imagen": "/Yecana.webp"
+    },
+    {
+        "id": 64,
+        "name": "Zidra",
+        "time": 44,
+        "description": "Cuando la lleva un Pokemon, si sus PS bajan a la mitad, le curará automáticamente un cuarto de sus PS totales.",
+        "seed1": "Muy Dulce",
+        "seed2": "Muy Amarga",
+        "seed3": "Muy Acida",
+        "imagen": "/Zidra.webp"
+    },
+]
